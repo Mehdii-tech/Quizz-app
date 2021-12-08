4deb8670b5bda1c94701e86c461b25b29021183d
Mehdi-githu <mehdi.jabbour@efrei.net>
2021-12-08 02:44:01 +0100

    'createquestion'

--- models/Quiz.ts
+++ models/Quiz.ts
@@ -50,6 +50,24 @@ import prisma from '../helpers/prisma'
 //     },
 // ];
 
+export const createQuestion = async({category, type, difficulty, question, correct_answer, incorrect_answer, authorId}:any)=>{
+    console.log(authorId,'yes')
+    const create = await prisma.question.create({
+      data: {
+        category:category,
+        type:type,
+        difficulty: difficulty,
+        question: question,
+        correct_answer: correct_answer,
+        incorrect_answer: incorrect_answer,
+        // quizz:{connect:{id:quizz.map(quizz=>quizz.id)}},
+        User: { connect: { id: authorId } },
+      },
+    })
+    console.log(create)
+    return create
+}
+
 export const find = async () =>{ const quizzes = await prisma.quizz.findMany({
 
 }); return quizzes} ;
---
+++ pages/api/ask/create.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import * as Quiz from "../../../models/Quiz";
+
+export default async function handle(req:NextApiRequest, res:NextApiResponse){
+    const {   
+        category,
+        type  ,          
+        difficulty ,      
+        question ,        
+        correct_answer  , 
+        incorrect_answer, authorId } = req.body;
+    if (req.method=="POST"){
+        try{
+            res.json({
+                data:{
+                    result: await Quiz.createQuestion(req.body)
+                }
+            })
+
+        }catch(err){
+            res.json({error:err})
+        }
+
+    }else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route. ${res.json(404)}`
+    );}
+}
